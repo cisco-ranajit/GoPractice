1078ae64ac31e4bea0a8111d1e02e6bb7c2a2321
cisco-ranajit <ranajitkhandual@gmail.com>
2022-11-22 19:04:23 +0530

    comparison operators

---
+++ programmes/comparison.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		a, b int
+	)
+	fmt.Scanln(&a)
+	fmt.Scanln(&b)
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+	fmt.Println(a >= b)
+	fmt.Println(a <= b)
+}
